80429cf1739ce87264709f8f2a9e0ef4b98d4810
gavinkentros <gavinkent622@gmail.com>
2026-07-23 20:23:16 -0700

    Finished implementing client entity and created Portfolio.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,21 +1,107 @@
 package com.wellsfargo.counselor.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Client {
 
     @Id
     @GeneratedValue
-    private long ClientId;
+    private long clientId;
 
+    @ManyToOne
+    @JoinColumn(name = "advisorId")
+    private Advisor advisor;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Date dateOfBirth;
 
     protected Client() {
 
     }
 
+    public Client (String firstName, String lastName, Advisor advisor, String address, String phone, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.advisor = advisor;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth (Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 
 }
